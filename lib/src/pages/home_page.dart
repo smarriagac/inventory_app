dbbd434385dafdb387dff1eb463fc1a9bac20043
Sebastian Marriaga <semcas95@gmail.com>
2021-01-09 00:43:13 -0500

    Inicio de la barra de navegacion

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -7,6 +7,24 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Text('Hola home page'),
       ),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+    );
+  }
+
+  _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      onTap: (index) {},
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones'),
+        ),
+      ],
     );
   }
 }
