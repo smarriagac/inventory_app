f115458c3771bb2c048242598982928ef347cf18
Sebastian Marriaga <semcas95@gmail.com>
2021-01-09 03:10:38 -0500

    sin funcionar....

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,7 +13,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentIndex = 0;
-  Future<String> _barcodeString;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
