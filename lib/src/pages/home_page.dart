77292b88467f395c25a7d2e61545c1db10d5041f
Sebastian Marriaga <semcas95@gmail.com>
2021-01-09 01:07:33 -0500

    finalizacion de navigationBa, implementacion de floatbotton

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:rickpan_app/src/pages/direcciones_page.dart';
+import 'package:rickpan_app/src/pages/mapas_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola home page'),
-      ),
+      body: _cargarPage(currentIndex),
       bottomNavigationBar: _crearBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: () {},
+      ),
     );
+  }
+
+  Widget _cargarPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      //break;
+      default:
+        return MapasPage();
+    }
   }
 
   _crearBottomNavigationBar() {
     return BottomNavigationBar(
-      currentIndex: 0,
-      onTap: (index) {},
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
       items: [
         BottomNavigationBarItem(
           icon: Icon(Icons.map),
