c01b6af3b5664fcc6e783b3980b71921bfd24832
Sebastian Marriaga <semcas95@gmail.com>
2021-01-10 02:07:35 -0500

    eliminar datos de la bade se datos

--- lib/src/pages/mapas_page.dart
+++ lib/src/pages/mapas_page.dart
@@ -20,12 +20,19 @@ class MapasPage extends StatelessWidget {
 
         return ListView.builder(
             itemCount: scans.length,
-            itemBuilder: (context, i) => ListTile(
-                  leading: Icon(Icons.cloud_queue,
-                      color: Theme.of(context).primaryColor),
-                  title: Text(scans[i].valor),
-                  trailing:
-                      Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+            itemBuilder: (context, i) => Dismissible(
+                  key: UniqueKey(),
+                  background: Container(color: Colors.red),
+                  onDismissed: (direction) =>
+                      DBprovider.db.deleteScan(scans[i].id),
+                  child: ListTile(
+                    leading: Icon(Icons.cloud_queue,
+                        color: Theme.of(context).primaryColor),
+                    title: Text(scans[i].valor),
+                    subtitle: Text('ID: ${scans[i].id}'),
+                    trailing:
+                        Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+                  ),
                 ));
       },
     );
